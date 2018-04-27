d2ae503fdce07e82ae3d52a66d465c7f133e312e
deepakkasera <deepak.kasera143.dk@gmail.com>
2018-04-28 01:34:36 +0530

    Genetic Algorithm

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -2,6 +2,8 @@
  * Created by deepak on 27/4/18.
  */
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.util.*;
 import java.io.*;
 import javax.imageio.ImageIO;
@@ -14,6 +16,10 @@ class Circle{
     public int red,green,blue;          //RGB values for the color of the circle
     public int alpha;                   //Alpha value of the color(Opacity and transparency factor)
     public int time;                    //the time when this circle was added to the member to which it belongs
+
+    public Circle() {
+
+    }
 
     public boolean isPointInside(int i,int j){
         return ((this.x - i) * (this.x - i) - (this.y - j) * (this.y - j) <= this.radius * this.radius);
@@ -77,8 +83,119 @@ class Genotype{
             }
         }
 
+        Iterator it = treeMap.entrySet().iterator();
+        double color_red = bg_red, color_green = bg_green, color_blue = bg_blue, color_alpha = bg_alpha;
+
+        while(it.hasNext()){
+            Map.Entry entry = (Map.Entry) it.next();
+            Circle circle = (Circle) entry.getValue();
+            double t = (double) (circle.alpha / 255.0);
+            double new_alpha = (double) (t + (1.0 - t) * color_alpha);
+
+            color_red = (t*circle.red + (1.0 - t) * color_red * color_alpha)/new_alpha;
+            color_green = (t*circle.green + (1.0-t)*color_green*color_alpha)/new_alpha;
+            color_blue = (t*circle.blue + (1.0-t)*color_blue*color_alpha)/new_alpha;
+            color_alpha = new_alpha;
+        }
+
+        Color color = new Color((int)color_red, (int)color_green, (int)color_blue, (int)(255*color_alpha) );
+
+        return color;
+    }
+
+
+    public int idnt(int diff) {
+        if (diff == 0)
+            return 1;
+        return 0;
+    }
+
+    public double getFitness(int[][] result){
+        double ans = 0;
+        for(int i = 0; i < result.length; i++){
+            for(int j = 0; j < result[0].length; j++){
+                Color m = getColorOfPoint(i,j);
+
+                Color n = new Color(result[i][j]);
+
+                ans += ( Math.pow(2,abs(m.getRed() - n.getRed())/255.0) +
+                         Math.pow(2,abs(m.getBlue() - n.getBlue())/255.0) +
+                         Math.pow(2,abs(m.getGreen() - n.getGreen())/255.0) );
+            }
+        }
+        this.fitness = 100000.0 / ans;
+        return this.fitness;
+    }
+
+    public void Print(){
+
     }
 }
 
 public class GeneticAlgorithm {
+
+
+    public static int POPSIZE = 15;             // Population size
+    public static int MAXGENS = 150000;         // number of generations
+    public static double PXOVER = 0.8;          //Probability of Crossover
+    public static double PMUTATION = 0.1;       //Probability of Mutation
+    public static Genotype population[];
+    public static Genotype newpopulation[];
+    public static int row, col;
+    public static int circleCount = 50;         // Number of circles a member of a population can have
+    public static int[][] result;
+    public static int RADIUSLIMIT=175;
+    public static double PMUTATIONCOLOR = .2;
+
+    //Function to convert an image to 2D matrix
+    //source for writing this function :- https://stackoverflow.com/a/17175454
+    private static int[][] convertTo2D(BufferedImage image) {
+        final byte[] pixels = ((DataBufferByte)image.getRaster().getDataBuffer()).getData();
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+        int[][] result = new int[height][width];
+
+        //getAlphaRaster() function returns null if if there is no alpha channel associated with the ColorModel in this image
+        //getAlphaRaster() -> a WritableRaster or null if this BufferedImage has no alpha channel associated with its ColorModel.
+        if(image.getAlphaRaster() == null){
+            final int pixelLength = 3;
+            for(int pixel = 0, row = 0, col = 0; pixelLength < pixels.length; pixel += pixelLength){
+                int argument = 0;
+                argument += ((int)pixels[pixel + 1] & 0xff);            //blue
+                argument += (((int)pixels[pixel + 2] & 0xff ) << 8);    //green
+                argument += (((int)pixels[pixel + 3] & 0xff ) << 16);   //red
+                result[row][col] = argument;
+                col++;
+                if(col == width){
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        else{
+            final int pixelLength = 3;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argument = 0;
+                argument += ((int) pixels[pixel] & 0xff);				   // blue
+                argument += (((int) pixels[pixel + 1] & 0xff) << 8);		// green
+                argument += (((int) pixels[pixel + 2] & 0xff) << 16);	   // red
+                result[row][col] = argument;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+//        return;
+        return result;
+    }
+
+    
+
+//    public static void convertColorArrayToImage(){
+//        File ImageFile = new File();
+//    }
+
 }
